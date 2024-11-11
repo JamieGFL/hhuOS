036bfb012a1d7b1d4df0931b7b34d45018e7dc36
JamieGFL <jamie.gerlach1337@gmail.com>
2024-11-11 19:06:30 +0100

    add cartridge and ppu files

---
+++ src/application/nes/cartridge.h
@@ -0,0 +1,11 @@
+#ifndef HHUOS_CARTRIDGE_H
+#define HHUOS_CARTRIDGE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif //HHUOS_CARTRIDGE_H
---
+++ src/application/nes/ppu2C02.h
@@ -0,0 +1,11 @@
+#ifndef HHUOS_PPU2C02_H
+#define HHUOS_PPU2C02_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif //HHUOS_PPU2C02_H
